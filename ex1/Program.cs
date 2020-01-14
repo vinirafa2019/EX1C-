a7b4c18dc28ecc9d0e545c973767b7e92e3745b4
vinirafa2019 <vinirafa2013@hotmail.com>
2020-01-14 16:35:34 -0300

    EX1-C#

---
+++ ex1/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o numero A");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o numero B");
+            int b = int.Parse(Console.ReadLine());
+
+            int medAri = 0;
+            int vt = 0;
+
+            if (a >= 0) {
+                if (b >= 0)
+                {
+                    if (a < b)
+                    {
+                        
+                        for (int i = a; i <= b; i++)
+                        {
+                            if (i % 2 == 0)
+                            {
+                                Console.WriteLine("");
+                            }
+                            else
+                            {
+                                vt = vt + i;
+                            }
+                        }
+                            Console.WriteLine("o valor total é"+ vt);
+                    }
+                    if (a > b)
+                    {
+                        for(int i = a; i <= b; i++)
+                        {
+                            if(i % 3 == 0)
+                            {
+                                medAri = medAri + i;
+                                
+                            }
+                        }
+                        Console.WriteLine("A media é: " + medAri);
+                    }
+                }
+            }
+        }
+    }
+}
